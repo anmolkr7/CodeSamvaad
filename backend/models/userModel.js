1450e08402f872930abfec4cb14d52a245630290
anmolkr7 <anmolojha952@gmail.com>
2025-03-26 21:11:38 +0530

    User Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+import * as bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        minLength:[6,'Email must be at least 6 characters long'],
+        maxLength:[50,'Email must not exceed 50 characters']
+    },
+    password:{
+        type:String,
+        select:false
+    }
+})
+
+userSchema.statics.hashPassword=async function(password){
+    return await bcrypt.hash(password,10);
+}
+
+userSchema.methods.isValidPassword=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateJWT=function(){
+    return jwt.sign({email:this.email},process.env.JWT_SECRET)
+}
+const User=mongoose.model('user',userSchema);
+
+export default User
