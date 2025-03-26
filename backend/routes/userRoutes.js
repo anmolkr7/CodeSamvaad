963e4fedee510cfb9ef490b7a5a987c69e1fe3a5
anmolkr7 <anmolojha952@gmail.com>
2025-03-26 21:36:57 +0530

    Creating user register

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import * as userController from '../controllers/userController.js'
+import { body } from "express-validator";
+const router=Router();
+
+
+router.post('/register',
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').isLength({min:6}).withMessage('Password must be at least 6 characters'),
+    userController.createUserController)
+
+    
+export default router;
