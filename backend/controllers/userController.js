e803f59eeaffd722d3f137f4580fa8bff3044b29
anmolkr7 <anmolojha952@gmail.com>
2025-03-26 23:46:02 +0530

    Login Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,3 +19,34 @@ export const createUserController=async(req,res)=>{
         res.status(400).send(error.message);
     }
 }
+
+export const loginController=async(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty())
+    {
+        return res.status(400).json({errors:errors.array()})
+    }
+    try{
+        const {email,password}=req.body
+        const user=await userModel.findOne({email}).select('+password')
+        if(!user)
+        {
+            return res.status(401).json({
+                errors:'Invalid Credentials'
+            })
+        }
+        const isMatch=await user.isValidPassword(password)
+        if(!isMatch)
+        {
+            return res.status(401).json({
+                errors:'Invalid password'
+            })
+        }
+        const token=await user.generateJWT();
+        res.status(200).json({user,token})
+
+    }
+    catch(err){
+        res.status(400).send(err.message)
+    }
+}
