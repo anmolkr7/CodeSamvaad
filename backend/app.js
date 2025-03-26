38908bb3976d1698d7a408b014c0b88801898601
anmolkr7 <anmolojha952@gmail.com>
2025-03-26 21:37:29 +0530

    userroutes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import connect from './db/db.js';
+import userRoutes from './routes/userRoutes.js'
 
 
 const app=express()
@@ -7,7 +8,7 @@ connect();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-
+app.use('/users',userRoutes)
 app.get('/',(req,res)=>{
     res.send("Hello world")
 })
