63e837576c6a6bceb6b56ed8e04fa168935249c3
anmolkr7 <anmolojha952@gmail.com>
2025-03-26 21:00:53 +0530

    connecting to mongo

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import connect from './db/db.js';
 
-connect();
-const app=express()
 
+const app=express()
+connect();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
