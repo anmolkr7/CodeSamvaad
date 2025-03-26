f08cfb535e567aaa87ac3c435095fc7aefb93d84
anmolkr7 <anmolojha952@gmail.com>
2025-03-26 21:17:09 +0530

    Creating User

---
+++ backend/services/userService.js
@@ -0,0 +1,14 @@
+import userModel from '../models/userModel.js'
+
+export const createUser=async({
+    email,password
+})=>{
+    if(!email || !password)
+        throw new Error("Email and password are required");
+    const hashedPassword=userModel.hashPassword(password)
+    const user=await userModel.create({
+        email,
+        password:hashedPassword
+    });
+    return user;   
+}
