4ea1a42074251899ac29b764e19d069683d9e44f
anmolkr7 <anmolojha952@gmail.com>
2025-03-28 19:19:35 +0530

    Cookie-parser

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,18 @@
 import express from 'express'
 import connect from './db/db.js';
 import userRoutes from './routes/userRoutes.js'
-
+import cookieParser from 'cookie-parser';
 
 const app=express()
 connect();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+
+app.use(cookieParser())
 app.use('/users',userRoutes)
 app.get('/',(req,res)=>{
     res.send("Hello world")
 })
 
-export default app;
+export default app; 
