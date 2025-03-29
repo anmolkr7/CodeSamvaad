1f41353fb86bcd655219faec3af0563aba7f2dcc
anmolkr7 <anmolojha952@gmail.com>
2025-03-29 16:23:38 +0530

    cors error

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,10 @@ import express from 'express'
 import connect from './db/db.js';
 import userRoutes from './routes/userRoutes.js'
 import cookieParser from 'cookie-parser';
-
+import cors from 'cors';
 const app=express()
 connect();
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
