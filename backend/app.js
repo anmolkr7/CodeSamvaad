a1439edeaa65a4e1d7387401be08f6b0b1a5b899
anmolkr7 <anmolojha952@gmail.com>
2025-04-02 15:55:56 +0530

    nothing

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,5 @@ app.use(express.urlencoded({extended:true}))
 
 app.use(cookieParser())
 app.use('/users',userRoutes)
-app.get('/',(req,res)=>{
-    res.send("Hello world")
-})
 
 export default app; 
