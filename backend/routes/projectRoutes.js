9822c0f66f8b2599d9df86bb7835adda7eddd4dd
anmolkr7 <anmolojha952@gmail.com>
2025-04-02 22:21:24 +0530

    route to create project

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { body } from "express-validator";
+import * as projectController from "../controllers/projectController.js";
+import * as authMiddleware from "../middlewares/authMiddleware.js";
+const router=Router();
+
+router.post('/create',
+    authMiddleware.authUser,
+    body('name').isString().withMessage('Name is required'),
+    projectController.createProject
+)
+
+
+export default router;
