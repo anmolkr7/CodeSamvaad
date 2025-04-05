5417131c806667dd826cfa993a35fb274bc123b5
anmolkr7 <anmolojha952@gmail.com>
2025-04-05 14:13:10 +0530

    fixed scroll overflow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import AppRoutes from './routes/AppRoutes'
 import { UserProvider } from './context/userContext'
+import Navbar from './components/Navbar'
 function App() {
 
   return (
     <UserProvider>
+    <div className="flex flex-col h-screen">
+    <Navbar />
+    <div className="flex-grow overflow-hidden">
       <AppRoutes />
+    </div>
+    </div>
     </UserProvider>
-    
   )
 }
 
