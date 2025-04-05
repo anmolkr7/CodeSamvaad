a57d83a1316984b22427719830ef14c2b9a3a2e1
anmolkr7 <anmolojha952@gmail.com>
2025-04-05 15:10:20 +0530

    side panel added

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import { useLocation} from "react-router-dom";
 const Project=() => { 
     const location=useLocation()
+
+    const [isSidePanelOpen,setIsSidePanelOpen]=useState(false)
+
     console.log(location.state)
     return (
         <main className="h-full w-full flex">
-            <section className="left flex flex-col h-full min-w-72 bg-slate-300">
+            <section className="left flex flex-col h-full min-w-72 bg-slate-300 relative">
                 <header className="flex justify-end p-2 px-4 w-full bg-slate-100">
-                    <button className="p-2">
+                    <button onClick={()=>setIsSidePanelOpen(!isSidePanelOpen)} className="p-2">
                     <i className="ri-group-fill"></i>
                     </button>
                 </header>
                 <div className="conversation-area flex flex-col flex-grow">
-                    <div className="message-box flex-grow flex flex-col gap-1">
+                    <div className="message-box p-1 flex-grow flex flex-col gap-1">
                         <div className="incoming-message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
                             <small className="opacity-65 text-xs">example@gmail.com</small>
-                            <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, dolores.</p>
+                            <p className="text-sm">Lorem lorem20ipsum dolor sit amet consectetur adipisicing elit. Omnis, dolores.</p>
                         </div>
                         <div className="outgoing-message ml-auto max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
                             <small className="opacity-65 text-xs">example@gmail.com</small>
@@ -28,6 +32,15 @@ const Project=() => {
                             <i className="ri-send-plane-fill text-xl"></i>
                         </button>
                     </div>
+                </div>
+                <div className={`absolute top-0 left-0 h-full w-72 bg-slate-200 z-20
+                transition-transform duration-300 ease-in-out
+                ${isSidePanelOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                <div className="flex justify-end p-2">
+                    <button onClick={()=>setIsSidePanelOpen(false)} className="text-xl">
+                        <i className="ri-close-line"></i>
+                    </button>
+                </div>
                 </div>
             </section>
         </main>
