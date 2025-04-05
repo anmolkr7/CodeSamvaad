a26ed2e5cde7c07b306271b2e9ee02af1b9e0c47
anmolkr7 <anmolojha952@gmail.com>
2025-04-05 14:16:18 +0530

    upon clicking a proejct it routes to project screen

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import {Route,BrowserRouter as Router,Routes} from 'react-router-dom'
 import Login from '../screens/Login'
 import Register from '../screens/Register'
 import { Home } from '../screens/Home.jsx'
+import Project from '../screens/Project.jsx'
 const AppRoutes = () => {
   return (
     <Router>
@@ -10,6 +11,7 @@ const AppRoutes = () => {
             <Route path="/" element={<Home/>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register/>} />
+            <Route path="/project" element={<Project/>} />
         </Routes>
     </Router>
   )
