41fb9f92cc22d6008a1e7b99e2ce8f06fd70b08a
anmolkr7 <anmolojha952@gmail.com>
2025-04-06 21:37:57 +0530

    modified middleware to validate projectId and fixed projectId string issue

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ import app from "./app.js";
 import http from 'http'
 import { Server } from 'socket.io';
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+import projectModel from './models/projectModel.js'
+
+
 const port=process.env.PORT || 3000;
 const server=http.createServer(app);
 
@@ -13,9 +17,16 @@ const io =new Server(server,{
     }
 })
 
-io.use((socket,next)=>{
+io.use(async (socket,next)=>{
     try{
         const token=socket.handshake.auth?.token ||socket.handshake.headers.authorization?.split(" ")[1]
+        const projectId=socket.handshake.query.projectId;
+        if(!mongoose.Types.ObjectId.isValid(projectId)){
+            return next(new Error("Invalid project id"))
+        }
+        
+        socket.project=await projectModel.findById(projectId)
+
         if(!token){
             return next(new Error("Authentication error"))
         }
@@ -32,7 +43,16 @@ io.use((socket,next)=>{
 })
 
 io.on('connection', socket => {
+    socket.roomId=socket.project._id.toString()
     console.log('a user connected');
+
+    socket.join(socket.roomId)
+
+    socket.on('project-message', (data) => {
+        console.log(data)
+        socket.broadcast.to(socket.roomId).emit('project-message', data);
+    }); 
+
     socket.on('event', data => { /* … */ });
     socket.on('disconnect', () => { /* … */ });
 });
