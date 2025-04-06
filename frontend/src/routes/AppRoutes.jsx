80cdb95990a75e38e89b2257c7b431be8bee3305
anmolkr7 <anmolojha952@gmail.com>
2025-04-06 21:36:30 +0530

    wrapping home and project screen comp with userauth

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,14 +4,15 @@ import Login from '../screens/Login'
 import Register from '../screens/Register'
 import { Home } from '../screens/Home.jsx'
 import Project from '../screens/Project.jsx'
+import UserAuth from '../auth/userAuth.jsx'
 const AppRoutes = () => {
   return (
     <Router>
         <Routes>
-            <Route path="/" element={<Home/>} />
+            <Route path="/" element={<UserAuth><Home/></UserAuth>} />
             <Route path="/login" element={<Login/>} />
             <Route path="/register" element={<Register/>} />
-            <Route path="/project" element={<Project/>} />
+            <Route path="/project" element={<UserAuth><Project/></UserAuth>} />
         </Routes>
     </Router>
   )
