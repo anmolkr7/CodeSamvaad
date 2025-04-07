de6f25b78851f55aff3b46581a8a64412a049de5
anmolkr7 <anmolojha952@gmail.com>
2025-04-07 22:18:11 +0530

    disconnect then leave room

--- backend/server.js
+++ backend/server.js
@@ -53,8 +53,11 @@ io.on('connection', socket => {
         socket.broadcast.to(socket.roomId).emit('project-message', data);
     }); 
 
-    socket.on('event', data => { /* … */ });
-    socket.on('disconnect', () => { /* … */ });
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        socket.leave(socket.roomId)
+    });
 });
 
 
