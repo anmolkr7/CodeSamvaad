d14871a34b70177d527e76ef1a8ec9f949f007ab
anmolkr7 <anmolojha952@gmail.com>
2025-04-08 22:08:54 +0530

    ai routes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ import express from 'express'
 import connect from './db/db.js';
 import userRoutes from './routes/userRoutes.js'
 import cookieParser from 'cookie-parser';
-import projectRoutes from './routes/projectRoutes.js'   
+import projectRoutes from './routes/projectRoutes.js' 
+import aiRoutes from './routes/aiRoutes.js'  
 import cors from 'cors';
 const app=express()
 connect();
@@ -13,4 +14,5 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use('/users',userRoutes)
 app.use('/projects',projectRoutes)
+app.use('/ai',aiRoutes)
 export default app; 
