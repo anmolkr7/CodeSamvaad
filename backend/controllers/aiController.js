ba2c8c3cbf9784df2e6f13003b17bd6cd0354e04
anmolkr7 <anmolojha952@gmail.com>
2025-04-08 22:10:25 +0530

    extracts prompt from query and sends to aiservice to get ooutput

---
+++ backend/controllers/aiController.js
@@ -0,0 +1,12 @@
+import * as ai from "../services/aiService.js";
+
+export const getResult=async (req, res) => {
+    
+    try {
+        const { prompt } = req.query;
+        const result = await ai.generateResult(prompt);
+        res.send(result);
+    } catch (error) {
+        return res.status(500).json({ message:error.message });
+    }
+}
